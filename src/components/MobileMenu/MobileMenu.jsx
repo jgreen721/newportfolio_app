47577dc2be59d0c16e3e9837b74686fda0c05742
Justin Green <justingreen@Justins-MacBook-Air.local>
2023-01-27 12:52:14 -0800

    save

---
+++ src/components/MobileMenu/MobileMenu.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./MobileMenu.css"
+
+const MobileMenu = () => {
+  return (
+    <div>MobileMenu</div>
+  )
+}
+
+export default MobileMenu
--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,5 @@
 export { default as Navbar } from "./Navbar/Navbar";
+export { default as MobileMenu } from "./MobileMenu/MobileMenu";
 export { default as CurrentPageMarker } from "./CurrentPageMarker/CurrentPageMarker";
 export { default as Hero } from "./Hero/Hero";
 export { default as About } from "./About/About";
